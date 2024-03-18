8430028e334fccd8ea4fa0d22bab69e416f416d3
Marcos <133986559+markakop@users.noreply.github.com>
2024-03-18 11:15:44 +0100

    dale

---
+++ eventos.php
@@ -0,0 +1,248 @@
+<div class="titulo">
+    EVENTOS
+</div>
+<div class="eventos">
+    <div class="evento-container">
+        <div class="event-img">
+            <img class="img-fest" src="./img/event_arenalSound.jpg" alt="">
+        </div>
+        <div class="texts">
+            <div class="event-titulo">
+                Arenal sound
+            </div>
+            <div class="sitio">
+                Castellon
+            </div>
+            <div class="fecha">
+                2 Agosto 2024
+            </div>
+            <div class="precio">
+                Desde 50$
+                <div class="button-event">
+                    <a class="button-event-red" href="">Quiero ir</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="evento-container">
+        <div class="event-img">
+            <img class="img-fest" src="./img/event_bigSound.jpg" alt="">
+        </div>
+        <div class="texts">
+            <div class="event-titulo">
+
+            </div>
+            <div class="sitio">
+
+            </div>
+            <div class="fecha">
+
+            </div>
+            <div class="precio">
+
+            </div>
+        </div>
+    </div>
+
+    <div class="evento-container">
+        <div class="event-img">
+            <img class="img-fest" src="./img/event_interestelar.jpg" alt="">
+        </div>
+        <div class="texts">
+            <div class="event-titulo">
+
+            </div>
+            <div class="sitio">
+
+            </div>
+            <div class="fecha">
+
+            </div>
+            <div class="precio">
+
+            </div>
+        </div>
+    </div>
+
+    <div class="evento-container">
+        <div class="event-img">
+            <img class="img-fest" src="./img/event_OrigenFest.jpg" alt="">
+        </div>
+        <div class="texts">
+            <div class="event-titulo">
+
+            </div>
+            <div class="sitio">
+
+            </div>
+            <div class="fecha">
+
+            </div>
+            <div class="precio">
+
+            </div>
+        </div>
+    </div>
+
+    <div class="evento-container">
+        <div class="event-img">
+            <img class="img-fest" src="./img/event_zueraSound.jpg" alt="">
+        </div>
+        <div class="texts">
+            <div class="event-titulo">
+
+            </div>
+            <div class="sitio">
+
+            </div>
+            <div class="fecha">
+
+            </div>
+            <div class="precio">
+
+            </div>
+        </div>
+    </div>
+
+    <div class="evento-container">
+        <div class="event-img">
+            <img class="img-fest" src="./img/event_arenalSound.jpg" alt="">
+        </div>
+        <div class="texts">
+            <div class="event-titulo">
+
+            </div>
+            <div class="sitio">
+
+            </div>
+            <div class="fecha">
+
+            </div>
+            <div class="precio">
+
+            </div>
+        </div>
+    </div>
+
+    <div class="evento-container">
+        <div class="event-img">
+            <img class="img-fest" src="./img/event_arenalSound.jpg" alt="">
+        </div>
+        <div class="texts">
+            <div class="event-titulo">
+
+            </div>
+            <div class="sitio">
+
+            </div>
+            <div class="fecha">
+
+            </div>
+            <div class="precio">
+
+            </div>
+        </div>
+    </div>
+
+    <div class="evento-container">
+        <div class="event-img">
+            <img class="img-fest" src="./img/event_arenalSound.jpg" alt="">
+        </div>
+        <div class="texts">
+            <div class="event-titulo">
+
+            </div>
+            <div class="sitio">
+
+            </div>
+            <div class="fecha">
+
+            </div>
+            <div class="precio">
+
+            </div>
+        </div>
+    </div>
+
+    <div class="evento-container">
+        <div class="event-img">
+            <img class="img-fest" src="./img/event_arenalSound.jpg" alt="">
+        </div>
+        <div class="texts">
+            <div class="event-titulo">
+
+            </div>
+            <div class="sitio">
+
+            </div>
+            <div class="fecha">
+
+            </div>
+            <div class="precio">
+
+            </div>
+        </div>
+    </div>
+
+    <div class="evento-container">
+        <div class="event-img">
+            <img class="img-fest" src="./img/event_arenalSound.jpg" alt="">
+        </div>
+        <div class="texts">
+            <div class="event-titulo">
+
+            </div>
+            <div class="sitio">
+
+            </div>
+            <div class="fecha">
+
+            </div>
+            <div class="precio">
+
+            </div>
+        </div>
+    </div>
+
+    <div class="evento-container">
+        <div class="event-img">
+            <img class="img-fest" src="./img/event_arenalSound.jpg" alt="">
+        </div>
+        <div class="texts">
+            <div class="event-titulo">
+
+            </div>
+            <div class="sitio">
+
+            </div>
+            <div class="fecha">
+
+            </div>
+            <div class="precio">
+
+            </div>
+        </div>
+    </div>
+
+    <div class="evento-container">
+        <div class="event-img">
+            <img class="img-fest" src="./img/event_arenalSound.jpg" alt="">
+        </div>
+        <div class="texts">
+            <div class="event-titulo">
+
+            </div>
+            <div class="sitio">
+
+            </div>
+            <div class="fecha">
+
+            </div>
+            <div class="precio">
+
+            </div>
+        </div>
+    </div>
+</div>
---
+++ footer.php
@@ -0,0 +1,58 @@
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h3>Conócenos</h3>
+                <ul style="list-style: none; padding-left: 10px;">
+                    <li><a href="?page=conocenos">Sobre Enterticket</a></li>
+                    <li><a href="?page=conocenos#comprar">Compra en Enterticket</a></li>
+                    <li><a href="?page=faq">Preguntas frecuentes</a></li>
+                    <li><a href="?page=faq#cambio_datos" target="_blank">Cambios en tus entradas</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h3>Prepara tu evento</h3>
+                <ul style="list-style: none; padding-left: 10px;">
+                    <li><a href="?page=prepara">Crea tu evento</a></li>
+                    <li><a href="?page=prepara">Por qué Enterticket</a></li>
+                    <li><a href="?page=prepara">Ticketing y accesos</a></li>
+                    <li><a href="https://promotor.enterticket.es/contacto">Contacta con nosotros</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h3>
+                    <a href="https://www.facebook.com/enterticket/" target="_blank" style="padding: 0px 10px;"><img src="images/si-facebook.png" width="16" style="-webkit-filter: brightness(1000%); -moz-filter: brightness(1000%); filter: brightness(1000%);"></a>
+                    <a href="https://www.twitter.com/enterticket/" target="_blank" style="padding: 0px 10px;"><img src="images/si-x.png" width="16" style="-webkit-filter: brightness(1000%); -moz-filter: brightness(1000%); filter: brightness(1000%);"></a>
+                    <a href="https://www.instagram.com/enterticket.es/" target="_blank" style="padding: 0px 10px;"><img src="images/si-instagram.png" width="16" style="-webkit-filter: brightness(1000%); -moz-filter: brightness(1000%); filter: brightness(1000%);"></a>
+                    <a href="https://www.youtube.com/c/Enterticket" target="_blank" style="padding: 0px 10px;"><img src="images/si-youtube.png" width="20" style="-webkit-filter: brightness(1000%); -moz-filter: brightness(1000%); filter: brightness(1000%);"></a>
+                </h3>
+                <ul style="list-style: none; padding-left: 10px;">
+                    <li><a href="?page=aviso_legal" target="_blank">Aviso legal</a></li>
+                    <li><a href="?page=condiciones" target="_blank">Términos y condiciones</a></li>
+                    <li><a href="?page=politica_cookies" target="_blank">Política de Cookies</a></li>
+                    <li><a href="?page=privacidad" target="_blank">Política de privacidad</a></li>
+                    <li><a href="?page=compromiso-proteccion-datos-personales" target="_blank">Compromiso de protección de
+                            datos</a></li>
+                    <li><a href="?page=conocenos#contacto" target="_blank">Contacto</a></li>
+                    <li><a href="?page=conocenos#trabaja-con-nosotros" target="_blank">Trabaja con nosotros</a></li>
+
+                    <li><a href="https://blog.enterticket.es" target="_blank">Enterticket Magazine</a></li>
+                    <li><a href="https://reservaplaya.es/" target="_blank">ReservaPlaya</a></li>
+                </ul>
+            </div>
+        </div>
+        <div style="padding: 20px;">
+            <div class="text-center">
+                <a href="https://bonoculturajoven.gob.es/" target="_blank">
+                    <img class="img-responsive" style="margin: auto; max-width: 200px; margin-bottom: 10px;" src="https://d31tcnbxvxtafg.cloudfront.net/images/public/logo_ministerio_bono_cultural.png">
+                </a>
+            </div>
+
+            <div class="text-center small">
+                © 2024 Entranet S.L.<br>
+                Todos los derechos reservados
+            </div>
+        </div>
+
+    </div>
+</div>
